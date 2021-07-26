9c44c06e55f7aee187359e5334974d9654054402
ivanovskiviktor <ivanovskiviktor3001@gmail.com>
2021-07-26 21:08:29 +0200

    Added necessary components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import BaseButton from './components/UI/BaseButton.vue';
+import BaseCard from './components/UI/BaseCard.vue';
+import BaseDialog from './components/UI/BaseDialog.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('base-button',BaseButton);
+app.component('base-card',BaseCard);
+app.component('base-dialog',BaseDialog);
+
+app.mount('#app');
